d21b6a4bb58cec6699fca2946d35db411a1c3994
Florian Kirsch <floriankirsch@opencsg.org>
2010-01-02 22:49:26 +0100

    OpenCSG 1.2.0 release

--- src/stencilManager.h
+++ src/stencilManager.h
@@ -1,5 +1,5 @@
 // OpenCSG - library for image-based CSG rendering for OpenGL
-// Copyright (C) 2002-2009, Florian Kirsch,
+// Copyright (C) 2002-2010, Florian Kirsch,
 // Hasso-Plattner-Institute at the University of Potsdam, Germany
 //
 // This library is free software; you can redistribute it and/or 
