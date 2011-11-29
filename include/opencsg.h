03bc6176ad25efac3d763564b9604af14d572d6f
Florian Kirsch <floriankirsch@opencsg.org>
2011-11-29 20:53:18 +0100

    Added a version number to opencsg.h

--- include/opencsg.h
+++ include/opencsg.h
@@ -28,6 +28,11 @@
 
 #include <vector>
 
+/// OpenCSG version. The version number has been introduced with version 1.3.2.
+/// Format is Major|Minor|Patch, each of them being 4-bit wide.
+#define OPENCSG_VERSION 0x0132
+#define OPENCSG_VERSION_STRING "OpenCSG 1.3.2"
+
 namespace OpenCSG {
 
     enum Operation { Intersection, Subtraction };
